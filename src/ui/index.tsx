8d37277bcbc0e7adc79a273bdbf343c284628ca4
Ivan Ushakov <ivan.ushakov@raiffeisen.ru>
2024-05-13 11:56:17 +0300

    feat: Forked and edit

---
+++ src/ui/index.tsx
@@ -0,0 +1,39 @@
+import { h, render } from 'preact';
+import FullUi from './components/FullUiComponent';
+
+if (window.customElements) {
+    window.customElements.define(
+        'module-federation-overrides-full',
+        preactCustomElement(FullUi, ['show-when-local-storage'])
+    );
+}
+
+function preactCustomElement(
+    Comp: () => h.JSX.Element,
+    observedAttributes: string[] = []
+) {
+    return class PreactCustomElement extends HTMLElement {
+        connectedCallback() {
+            this.renderWithPreact();
+        }
+        disconnectedCallback() {
+            render(null, this);
+            (this as any).renderedEl = null;
+        }
+        static get observedAttributes() {
+            return observedAttributes;
+        }
+        attributeChangedCallback() {
+            this.renderWithPreact();
+        }
+        renderWithPreact() {
+            (this as any).renderedEl = render(
+                h(Comp, { customElement: this }),
+                this,
+                (this as any).renderedEl
+            );
+
+            // (this as any).renderedEl =  render(Comp, document.body);
+        }
+    };
+}
